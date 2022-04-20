95f412f8e5e8316e4e03a9f5e6cf13870a0e1539
ecehn <eric.chen5@yahoo.com>
2022-04-20 21:09:07 +0000

    Sign-in create test

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.feature "Projects", type: :feature do
   context "Create new project" do
     before(:each) do
+      sign_in create(:user, name: 'John Doe')
       visit new_project_path
       within("form") do
         fill_in "Title", with: "Test title"
@@ -24,6 +25,7 @@ RSpec.feature "Projects", type: :feature do
   context "Update project" do
     let(:project) { Project.create(title: "Test title", description: "Test content") }
     before(:each) do
+      sign_in create(:user, name: 'John Doe')
       visit edit_project_path(project)
     end
 
@@ -47,6 +49,7 @@ RSpec.feature "Projects", type: :feature do
   context "Remove existing project" do
     let!(:project) { Project.create(title: "Test title", description: "Test content") }
     scenario "remove project" do
+      sign_in create(:user, name: 'John Doe')
       visit projects_path
       click_link "Destroy"
       expect(page).to have_content("Project was successfully destroyed")
