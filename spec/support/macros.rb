c97122b79edb5d249c36943634f521efd3bf1059
ecehn <eric.chen5@yahoo.com>
2022-04-20 23:21:31 +0000

    Removed request env in test sign in

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -1,7 +1,7 @@
 module IntegrationMacros
 
     def test_sign_in
-      @request.env["devise.mapping"] = Devise.mappings[:user]
+      #@request.env["devise.mapping"] = Devise.mappings[:user]
       user = FactoryBot.create(:user)
       sign_in user
     end
