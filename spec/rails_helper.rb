d5bdd73e9f6f30233ed09bafe57f00248a7a63c3
ecehn <eric.chen5@yahoo.com>
2022-04-20 22:41:37 +0000

    fixed require relative

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'devise'
-require_relative 'support/controller_macros'
+require_relative 'support/macros'
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
