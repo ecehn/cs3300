b06f4e95080348c5a54add18262a687beaf1a4de
ecehn <eric.chen5@yahoo.com>
2022-05-05 06:19:18 +0000

    Adjusted test for new css

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -57,7 +57,7 @@ RSpec.feature "Projects", type: :feature do
       user = FactoryBot.create(:user)
       sign_in user
       visit projects_path
-      click_link "Destroy"
+      click_button "Destroy"
       expect(page).to have_content("Project was successfully destroyed")
       expect(Project.count).to eq(0)
     end
