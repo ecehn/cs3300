bdda5d6374b9d59ceaa9e6b95f5e238aa437d336
ecehn <eric.chen5@yahoo.com>
2022-05-10 01:59:34 +0000

    Updated CircleCI heroku api again

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -92,6 +92,6 @@ SimpleCov.start 'rails' do
   add_filter '/spec/' # for rspec
   add_filter '/app/channels'
   add_filter 'app/jobs'
-  #add_filter 'app/helpers'
+  add_filter 'app/helpers'
   add_filter 'app/mailers'
 end
